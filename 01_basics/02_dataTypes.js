21f7a3de38263604c8e668960f1e8d8ced560cd1
Nikita Patil <133837683+nikitanpatil1@users.noreply.github.com>
2024-01-16 12:26:00 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,38 @@
+"use strict"; // after writing this it treats all js code as newer version
+
+//code redability should be high
+
+let name = "nikki"
+let age = 18
+let isLoggedIn = true
+
+//types of datatypes
+
+//primitive
+/*
+  - number => 2 to the power 53
+  - null => standalone value
+  - boolean => true/false
+  - bigint
+  - string => ""
+  - symbol => unique (when you have to compare if the component is unique 
+    then this is use i.e to find uniqueness)
+  - undefined => 
+*/
+
+
+//non primitive
+
+/* 
+  - object 
+*/
+
+
+//typeof
+
+console.log(typeof age)
+
+/* 
+ - typeof null = object
+ - typeof undefined = undefined
+*/
