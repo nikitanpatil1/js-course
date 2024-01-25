16ef4abc59a7d35717bfcfde0e368cfce1d914c0
Nikita Patil <133837683+nikitanpatil1@users.noreply.github.com>
2024-01-25 15:05:53 +0000

    prefix and postfix

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,15 +1,15 @@
 //conversions
 
-let variablename1 = Number(variablename1)
+// let variablename1 = Number(variablename1)
 /* "33" => 33
    "33abc" => NaN
    true => 1; false => 0
 */
 
 
-let variablename2 = String(variablename2)
+// let variablename2 = String(variablename2)
 
-let variablename3 = Boolean(variablename3)
+// let variablename3 = Boolean(variablename3)
 /*
   1 => true; 
   0 => false
@@ -41,3 +41,35 @@ let variablename3 = Boolean(variablename3)
 // but never write like this use () to seperate operations you want to run first  i.e  console.log( (3+4) * 5 % 3)
 
 
+
+// Prefix and Postfix
+
+/*1) increment
+    
+   postfix++ : increments and returns the value before incrementing i.e prints original value and then increments it.
+   ++prefix : increments and returns the value after incrementing i.e increments the value and prints the incremented value.
+*/
+  
+//postfix
+  let x = 3;
+  let y = x++;
+  console.log(`x:${x}, y:${y}`);
+//out: x:4, y:3
+  
+//prefix
+  let a = 3;
+  let b = ++a;
+  console.log(`a:${a}, b:${b}`);
+//out: a:4, b:4
+  
+  
+/*2) decrement
+
+  postfix++ : decrements and returns the value before decrementing i.e prints 
+  original value and then decrements it.
+  out: x:2, y:3 when x= 3
+
+
+  ++prefix : decrements and returns the value after decrementing i.e decrements the value and prints the decremented value.
+  out: x:2, y:2 when x = 3
+*/
