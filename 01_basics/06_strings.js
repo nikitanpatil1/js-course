87954117ee3f5f2b6a47d968b539e5bb6a1f986e
Nikita Patil <133837683+nikitanpatil1@users.noreply.github.com>
2024-02-07 13:29:27 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,16 @@
+
+const name = "nikita";
+const repoCount = 50;
+
+// - string interpolation i.e the right syntax to inject variable
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// - the use of writing like this is you can do many things on the go i.e can use any sring property there only ${name.toUpperCase()} 
+
+
+// * another way to declare the string i.e using "new" keyword i.e you use the js object..new string is creating a new object for it
+const gameName = new String('nikki');
+console.log(gameName)
+
+
+
+//string methods/ prototypes
