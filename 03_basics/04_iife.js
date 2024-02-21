36a66bc05f3d0138267971921c34bf1fee455407
Nikita Patil <133837683+nikitanpatil1@users.noreply.github.com>
2024-02-21 15:15:11 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expressions (IIFE)
+/* 
+  - Many times problem occurs because of the global scope polution, and to remove that global scopes variables/ declarations polution we use IIFE
+  - the function which imediately gets executed
+  - 
+
+  - syntax 
+    ()()
+    1st () - used to write the function definition
+    2nd () - used to call that function i.e execution call
+*/
