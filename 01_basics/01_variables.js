071784a845634fa0f6fbc880fcee02b53ac7ea60
Nikita Patil <133837683+nikitanpatil1@users.noreply.github.com>
2024-03-03 15:08:38 +0000

    bit changes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -10,5 +10,9 @@ console.table([accountId, accountEmail, accountPassword, accountCity, accountSta
 /* -you can update let and var
    -only var can be redefined
    -const can neither be updated nor redefined
-   -never use var, as  it gives issue in block and functional scope i.e if same variable name is present at two places that is one inside the block and one outside the block, then even if we changed the variable inside the block... the variable outside the block also used to get updated even tho we didn't wanted it to change.
-   -in js you can allot memory to an variable without even declaring it. but don't!  */
+   -never use var, as  it gives issue in block and functional scope 
+    i.e if same variable name is present at two places that is one inside the block and one outside the block, 
+    then even if we changed the variable inside the block... 
+    the variable outside the block also used to get updated even tho we didn't wanted it to change.
+   -in js you can allot memory to an variable without even declaring it. but don't!  
+*/
--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,14 +1,32 @@
-"use strict"; // after writing this it treats all js code as newer version
+"use strict"; 
+// after writing this it treats all js code as newer version
 
 //code redability should be high
+
+// ++++++++++++++++++++++++++
 
 let name = "nikki"
 let age = 18
 let isLoggedIn = true
 
+/* 
+   how the data is stored in the memory and how you can access that data...
+   on basis of this that data is classified into two types.
+*/
 
+/*
 
-// how the data is stored in the memory and how you can access that data...on basis of this that data is classified into two types.
+ + call by value
+   -The original variable is not modified on changes in other variables.
+   -Actual and copied variables will be created in different memory locations.
+   -On passing variables in a function, any changes made in the passed variable will not affect the original one.
+
+ + call by reference 
+   -The original variable gets modified on changes in other variables.
+   -Actual and copied variables are created in the same memory location.
+   -On passing variables in a function, any changes made in the passed parameter will update the original variable’s reference too.  
+
+*/
 
 //types of datatypes
 
@@ -30,8 +48,11 @@ let isLoggedIn = true
 
 /* 
   - object 
-   (array and functions are also objects ) so they are also non primitive 
-   typeof is object and of function is functions object)
+  - (array and functions are also objects ) 
+    so they are also non primitive...
+  - typeof  
+    array = object  
+    function = functions object
 */
 
 
