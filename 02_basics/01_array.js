d8477161e6a7957ae9da2830dfb69294b391176f
Nikita Patil <133837683+nikitanpatil1@users.noreply.github.com>
2024-03-11 09:39:40 +0000

    some changes

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,22 +1,35 @@
 // Arrays are collection of multiple items that are stored under a single variable name
 
-// JS arrays are resizable and can contain a mix of different datatypes i.e u can add new item in already existing array... and array can be 
+/* 
+  - JS arrays are resizable
+  - can contain a mix of different datatypes 
+    i.e u can add new item in already existing array... and array can be 
+*/
 const myArr = [1, 2, "niki", "pat", true];
 
-// JS arrays are not associative arrays i.e if u want to access an item from the array then u have to use its index number e.g- 
+
+/*  
+  - JS arrays are not associative arrays 
+  i.e if u want to access an item from the array then u have to use its index number 
+*/ 
+//e.g- 
 console.log(myArr[1]);
 
-// JS arrays are 0 indexed i.e index numbering starts from 0
 
-/* JavaScript array-copy operations create shallow copies i.e its a heap memory 
+// - JS arrays are 0 indexed i.e index numbering starts from 0
 
-- shallow copy = is a copy whose properties share the same references as those of the source object from which the copy was made.
+/* 
+  JavaScript array-copy operations create shallow copies i.e its a heap memory 
 
-- deep copy =  is a copy whose properties do not share the same references as those of the source object from which the copy was made.
+  - shallow copy = is a copy whose properties share the same references 
+                   as those of the source object from which the copy was made.
+
+  - deep copy = is a copy whose properties do not share the same references 
+                as those of the source object from which the copy was made.
 */
 
 
-// another ay to declare array
+// another way to declare array
 const myArr2 = new Array(1, 2, 3, 4)
 console.log(myArr2)
 
@@ -48,21 +61,29 @@ const arr = [1, 2, 3, 4, 5]
 arr.push(6); // out- [1, 2, 3, 4, 5, 6] add new item at the end 
 //push() pushes into existing array
 
-arr.pop(); // out- [1, 2, 3, 4, 5] removes the last item from array
+arr.pop(); // out- [1, 2, 3, 4, 5] 
+//removes the last item from array
 
-arr.unshift(0); // out- [0, 1, 2, 3, 4, 5] adds new item to the start of array
+arr.unshift(0); // out- [0, 1, 2, 3, 4, 5] 
+//adds new item to the start of array
 
-arr.shift(); // out- [1, 2, 3, 4, 5] removes the first item from the array
+arr.shift(); // out- [1, 2, 3, 4, 5] 
+//removes the first item from the array
 
-arr.includes(5); // out- true  it is used to ask question/ to check values in arrays
+arr.includes(5); // out- true  
+//it is used to ask question/ to check values in arrays
 
 arr.indexOf(5);
 
-arr.join(); // out- 1,2,3,4,5  converts the array on which applied to string by the way its called to join 
+arr.join(); // out- 1,2,3,4,5  
+//converts the array on which applied to string by the way its called to join 
   
 const myArr3 = new Array(1, 2, 3, 4, 5) 
-console.log(myArr3.slice(1, 3)) // out- [2, 3] and original array [1, 2, 3, 4, 5] i.e it prints the selected range and dosent manipulates original array.
+console.log(myArr3.slice(1, 3)) // out- [2, 3] and original array [1, 2, 3, 4, 5] 
+//i.e it prints the selected range and dosent manipulates original array.
 console.log(myArr3)
-console.log(myArr3.splice(1, 3)) // out- [2, 3, 4] and original array [1, 5] i.e it prints the range including the next item also and it dose manipulates the original array.
+
+console.log(myArr3.splice(1, 3)) // out- [2, 3, 4] and original array [1, 5] 
+//i.e it prints the range including the next item also.. and it dose manipulates the original array.
 console.log(myArr3)
  
--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -5,27 +5,39 @@ const arr2 = ["A", "B", "C", "D"];
 
 // when you push() a array into an array
 // if you use .push method
-// arr1.push(arr2)
-// console.log(arr1);// out- [ 'a', 'b', 'c', 'd', [ 'A', 'B', 'C', 'D' ] ]
-// so if we push one array into another then that whole array gets combined as a single item in that first array i.e it took that array as a singel data
-// u should not use it
+arr1.push(arr2)
+console.log(arr1);// out- [ 'a', 'b', 'c', 'd', [ 'A', 'B', 'C', 'D' ] ]
+/* 
+ - so if we push one array into another then 
+ that whole array gets combined as a single item in that first array 
+ i.e it took that array as a single data
+ - so do not use this for two array combinition
+*/
 
 
 // using concat()
 const newConcat = arr1.concat(arr2);
-console.log(newConcat);// out- [ 'a', 'b', 'c','d', 'A', 'B','C', 'D'] i.e it combines two arrays and returns new array...i.e to get answer you will have to assign new variable.
-// there is even more easy method than concat...and you should use it.
+console.log(newConcat);// out- [ 'a', 'b', 'c','d', 'A', 'B','C', 'D'] 
+/*
+  i.e it combines two arrays and returns new array...
+  i.e to get answer you will have to assign new variable.
+  - there is even more easy method than concat...and you should use it.
+*/
 
 
 // spread operator
 
-const newSpread = [...arr1, ...arr2] // as you put ... then it no more remains array, its each elements have now become individual
+const newSpread = [...arr1, ...arr2] 
+// as you put "..." then it no more remains array, its each elements now has become individual
 console.log(newSpread) //out- ['a', 'b', 'c','d', 'A', 'B','C', 'D']
 
 
-// .flat() 
-//Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth.
-// one more method to solve complex array
+// .flat(depth) depth = Defaults to 1
+/*
+ - Returns a new array with all sub-array elements concatenated into it.. 
+   recursively up to the specified depth.
+ - one more method to solve complex array
+*/
 
 const complexArr = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
@@ -41,13 +53,23 @@ console.log(Array.isArray("Nikkita")) // out- false
 // Array.from
 console.log(Array.from("Nikkita")); // out- ['N', 'i', 'k','k', 'i', 't','a']
 
-// you can even convert object into array
-// but you cant just put the whole object i.e
+/* 
+  - you can even convert object into array
+  - but you can't just put the whole object i.e
+*/
 console.log(Array.from({name : "nikki"}));// out- []
-// it will give an empty array.. so when you want to convert obj to arr at that time you should specify if you want either "keys" or "values" to be converted into the array.
+/* 
+  it will give an empty array.. 
+  - so when you want to convert obj to arr 
+    at that time you should specify if you want either "keys" or "values" to be converted into the array.
+*/
 
 // Array.of()
-//A set of elements to include in the new array object.Returns a new array from a set of elements.
+/* 
+  - A set of elements to include in the new array object.
+  - Returns a new array from a set of elements.
+  - way to convert diff variables into array
+*/
 const num1 = 100;
 const num2 = 200;
 const num3 = 300;
