657a48b56a9618859204fa0dee2bd3ec6bef4230
Nikita Patil <133837683+nikitanpatil1@users.noreply.github.com>
2024-03-21 13:41:47 +0000

    EandR dom

--- 06_DOM/05_dom_edit&remove_elements.js
+++ 06_DOM/05_dom_edit&remove_elements.js
@@ -7,4 +7,58 @@ function addLanguage(langName){
     document.querySelector('.language').appendChild(li)
 }
 addLanguage("Python")
+addLanguage("typescript")
 // this method is ok but creats optimisation issue
+
+// 2nd method (using optimise way)
+function addOptiLanguage(langName){
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(langName))
+    document.querySelector('.language').appendChild(li)
+}
+addOptiLanguage("php")
+
+// in this method u dont need to traverse the whole tree
+//so both the methods are same just 2nd one is more optimised
+
+
+// *** Edit Values ******
+
+//1) using .innerHTML
+const secondLang = document.querySelector("li:nth-child(2)")
+secondLang.innerHTML = "Mojo"
+/*out- Javascript
+ Mojo
+ typescript
+ php
+*/
+
+
+//2) by creating new element and using .replaceWith()
+const newli = document.createElement('li')
+newli.textContent = "ruby"
+secondLang.replaceWith(newli)
+/*out- Javascript
+ ruby
+ typescript
+ php
+*/
+
+//3) by using .outerHTML
+const firstLang = document.querySelector("li:first-child")
+firstLang.outerHTML = '<li>C++</li>'
+/*out- C++
+ ruby
+ typescript
+ php
+*/
+
+
+//*** remove Values ***
+const lastLang = document.querySelector('li:last-child')
+lastLang.remove()
+/*out- C++
+ ruby
+ typescript
+
+*/
